eb19b4cd57a2382f8d7d379a0e86b5a3feea6285
Alex <little_al14@hotmail.com>
2017-12-09 16:13:19 -0400

    triangle decals added to hr on blogs

--- src/js/vue/blog.vue
+++ src/js/vue/blog.vue
@@ -1,6 +1,9 @@
 <style lang="less" scoped>
     @import "../../less/vars";
 
+    article > .columns {
+        position: relative;
+    }
     h3 {
         margin: 0;
     }
@@ -10,8 +13,27 @@
     section {
         margin-bottom: 4em;
     }
+    // hr + decals
     hr {
+        margin: 1em 0;
         border-width: 0.1rem;
+    }
+    .hr-decal {
+        @decal-size: 5px;
+        position: absolute;
+        width: 0;
+        height: 0;
+        border-top: @decal-size solid transparent;
+        border-bottom: @decal-size solid transparent;
+        bottom: 11px;
+
+        &.left {
+            border-right: @decal-size solid #E1E1E1;
+            right: 0;
+        }
+        &.right {
+            border-left: @decal-size solid #E1E1E1;
+        }
     }
 </style>
 
@@ -22,7 +44,10 @@
             <b>{{blog.date}}</b>
             <p v-html="blog.info"></p>
             <p class="content" v-html="blog.content"></p>
+            <div class="hr-decal left"></div>
+            <div class="hr-decal right"></div>
             <hr>
+
         </div>
     </article>
 </template>
